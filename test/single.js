f4a9cc73c334ef27256b7d46e8523d72ee7b8d69
Mathieu Sylvain <mathieu@ti-coco.com>
2012-05-13 11:34:29 -0400

    Separated code in multiple modules

--- test/single.js
+++ test/single.js
@@ -83,7 +83,7 @@ function cookTestFile(name) {
 
 console.time("tests");
 
-cookTestFile("partials-renderTag");
+//cookTestFile("partials-renderTag");
 
 cookTestFile("if");
 cookTestFile("if-else");
