761b9a6ec8090a6e1d61516ba2a46df92af4c5ec
Mathieu Sylvain <mathieu@ti-coco.com>
2012-06-02 12:25:46 -0400

    Fixed lost clusures when rendering child templates

--- build/buid.js
+++ build/buid.js
@@ -1,14 +1,7 @@
 var Cook = require("../lib");
-var fs = require("fs");
 
 var cook = new Cook();
+cook.options.rootPath = __dirname;
 
-//todo: option to set root of resolver
-cook.resolve = function (id) {
-	var str = "";
-	var buffer = fs.readFileSync(__dirname + "/" + id + ".cook");
-	if (buffer) str = buffer.toString();
-	return str;
-};
 var output = cook.get("cook").render({});
 console.log(output);
